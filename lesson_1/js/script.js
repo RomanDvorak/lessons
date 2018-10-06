7bd2bab0d608f854e8b0438741bb8afd7b4e5b5a
Roman <drvmoz@mail.ru>
2018-10-06 21:36:10 +0300

    Это исправленное задание

--- lesson_1/js/script.js
+++ lesson_1/js/script.js
@@ -12,11 +12,11 @@ let appData = {
   savings: false
 };
 let a1 = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    a3 = prompt("Во сколько обойдётся?", ""),
-    a2 = prompt("Введите обязательную статью расходов в этом месяце", ""),
+    a2 = prompt("Во сколько обойдётся?", ""),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце", ""),
     a4 = prompt("Во сколько обойдётся?", "");
-appData.expenses[a1] = a3;
-appData.expenses[a2] = a4;
+appData.expenses[a1] = a2;
+appData.expenses[a3] = a4;
 console.log(appData.expenses);
 
 document.write(money / 30);
