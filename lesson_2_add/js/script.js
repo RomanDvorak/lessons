b2e294db5f2c67f57d3a3adaf166fcdcaf5759ce
Roman <drvmoz@mail.ru>
2018-10-08 09:33:42 +0300

    Это основное задание

---
+++ lesson_2_add/js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/* let x = 5;
+alert( x++ ); */ // При каждом выделении и наводе мыши увеличивается число
+
+/* let test = [ ] + false - null + true;
+alert(test); */ // Равно NaN
+
+/* let y = 1;
+let x = y = 2;
+alert(x); */ // Выведет 2
+
+/* let test = [ ] + 1 + 2;
+alert(test) */ // Выведет 12
+
+/* alert( "1"[0] ); */ // Выведет 1
+
+/* let test = 2 && 1 && null && 0 && undefined;
+alert(test); */ // Выведет null
