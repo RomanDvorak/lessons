e3d5d4660206622498ce352a27f709aa568e5250
Roman <drvmoz@mail.ru>
2018-10-16 21:52:08 +0300

    Это дополнительное задание

--- lesson_7_hard/js/script.js
+++ lesson_7_hard/js/script.js
@@ -4,17 +4,51 @@ let btn = document.querySelector('.start')
 function myAnimation() {
   let pos = 0;
 
-  let id = requestAnimationFrame(frame);
-  function frame () {
+  let id1 = requestAnimationFrame(frameRightDown);
+  function frameRightDown () {
     if (pos == 250) {
-      clearInterval(id);
+      clearInterval(id1);
+      let id2 = requestAnimationFrame(frameLeft);
     } else {
-      pos++;
+      pos += 5;
       square.style.left = pos + 'px';
       square.style.top = pos + 'px';
-      id = requestAnimationFrame(frame);
+      id1 = requestAnimationFrame(frameRightDown);
     }
   }
+  function frameLeft () {
+    if (pos == 50) {
+      clearInterval(id2);
+      pos = +((square.style.top).slice(0,-2));
+      let id3 = requestAnimationFrame(frameTop);
+    } else {
+      pos -= 5;
+      square.style.left = pos + 'px';
+      id2 = requestAnimationFrame(frameLeft);
+    }
+  }
+  function frameTop () {
+    if (pos == 150) {
+      clearInterval(id3);
+      pos = +((square.style.left).slice(0,-2));
+      let id4 = requestAnimationFrame(frameCenter);
+    } else {
+      pos -= 5;
+      square.style.top = pos + 'px';
+      id3 = requestAnimationFrame(frameTop);
+    }
+  }
+  function frameCenter () {
+    if (pos == 150) {
+      clearInterval(id4);
+    } else {
+      pos += 5;
+      square.style.left = pos + 'px';
+      id4 = requestAnimationFrame(frameCenter);
+    }
+  }
+
+
 }
 
 btn.addEventListener('click', myAnimation);
